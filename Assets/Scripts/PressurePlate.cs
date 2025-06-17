21e37808a89b2ac9cf87b4b0e3bdfe6f90cc9bcb
Jarvis <joris.salvador@icloud.com>
2025-06-17 16:27:47 +0200

    first animation

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField] private Animator gateAnimator;
+    [SerializeField] private string openTrigger = "Open";
+    [SerializeField] private string closeTrigger = "Close";
+
+    private int objectsOnPlate = 0;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grabbable"))
+        {
+            objectsOnPlate++;
+            if (objectsOnPlate == 1)
+                gateAnimator.SetTrigger(openTrigger);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Grabbable"))
+        {
+            objectsOnPlate = Mathf.Max(0, objectsOnPlate - 1);
+            if (objectsOnPlate == 0)
+                gateAnimator.SetTrigger(closeTrigger);
+        }
+    }
+}
+// Ce script gère une plaque de pression qui ouvre et ferme une porte lorsque des objets sont placés ou retirés.
+// Il utilise un Animator pour contrôler les états d'animation de la porte en fonction du nombre d'objets sur la plaque.
