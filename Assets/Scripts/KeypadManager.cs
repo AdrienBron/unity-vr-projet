28b5392dab153d69575e141f3aa668cf4aea1b68
Adrien_Bron <adrienbron2003@gmail.com>
2025-06-18 16:59:45 +0200

    update keypade

--- Assets/Scripts/KeypadManager.cs
+++ Assets/Scripts/KeypadManager.cs
@@ -2,24 +2,19 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using TMPro;
 
 public class KeypadManager : MonoBehaviour
 {
     private string currentInput = "";
     public Animator anim;
+    public TMP_Text screen;
 
     public void ReceiveInput(string val)
     {
         currentInput += val;
+        screen.text = currentInput;
         Debug.Log("Code actuel : " + currentInput);
-
-        if (currentInput == "354")
-        {
-            Debug.Log("Code correct !");
-            anim.SetTrigger("open");
-            StartCoroutine("IEnumerator");
-
-        }
     }
 
     public IEnumerator loadScene()
@@ -29,5 +24,16 @@ public class KeypadManager : MonoBehaviour
 
     }
 
-    public void ResetInput() => currentInput = "";
+    public void EnterInput()
+    {
+        if (currentInput == "354")
+        {
+            Debug.Log("Code correct !");
+            anim.SetTrigger("open");
+            StartCoroutine("IEnumerator");
+
+        }
+        currentInput = "";
+        screen.text = currentInput;
+    }
 }
